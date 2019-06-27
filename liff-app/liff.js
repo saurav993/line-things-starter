d252975f83ad3ec4131035b565bd1e71925e16b0
saurav993 <44286491+saurav993@users.noreply.github.com>
2019-06-27 20:46:50 +0800

    Add files via upload

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -271,6 +271,23 @@ function liffGetButtonStateCharacteristic(characteristic) {
     // (Get notified when button state changes)
     characteristic.startNotifications().then(() => {
         characteristic.addEventListener('characteristicvaluechanged', e => {
+            const val = (new Uint8Array(e.target.value.buffer))[0];
+			uiTemp(val);
+            if (val > 0) {
+                // press
+                uiToggleStateButton(true);
+            } else {
+                // release
+                uiToggleStateButton(false);
+                uiCountPressButton();
+            }
+        });
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+	
+	characteristic.startNotifications().then(() => {
+        characteristic.addEventListener('characteristicvaluechanged', e => {
             const val2 = (new Uint8Array(e.target.value.buffer))[0];
 			uiHumid(val2);
             if (val2 > 0) {
