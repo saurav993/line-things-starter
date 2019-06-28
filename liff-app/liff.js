389c9e245d0dd825bda44ab396ba30584e0bde5d
saurav993 <44286491+saurav993@users.noreply.github.com>
2019-06-28 14:39:50 +0800

    Add files via upload

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -287,26 +287,6 @@ function liffGetButtonStateCharacteristic(characteristic) {
     });
 }
 
-function liffGetButtonStateCharacteristic(characteristic) {
-	characteristic.startNotifications().then(() => {
-        characteristic.addEventListener('characteristicvaluechanged', e => {
-            const val2 = (new Uint8Array(e.target.value.buffer))[0];
-			uiHumid(val2);
-            if (val2 > 0) {
-                // press
-                uiToggleStateButton(true);
-            } else {
-                // release
-                uiToggleStateButton(false);
-                uiCountPressButton();
-            }
-        });
-    }).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });
-	
-}
-
 function liffToggleDeviceLedState(state) {
     // on: 0x01
     // off: 0x00
